8cd56999d012d47c99f2e13c2e6a91a12f3b0e50
bfrest <freston75@gmail.com>
2018-08-25 13:13:29 -0600

    working on the admin and status components

--- front_end/src/components/Admin/Admin.js
+++ front_end/src/components/Admin/Admin.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Admin.css";
 
+import Status from "../Status/Status.js";
+
 class Admin extends Component {
   constructor() {
     super();
@@ -9,24 +11,41 @@ class Admin extends Component {
       firstName: "Brendon",
       lastName: "Freston",
       email: "freston75@gmail.com",
-      profileImg: "http://res.cloudinary.com/bfrest/image/upload/v1519392910/mainPic.png"
+      profileImg: "http://res.cloudinary.com/bfrest/image/upload/v1519392910/mainPic.png",
+      showStatus: false
     };
   }
 
+  showStatus() {
+    const { showStatus } = this.state;
+
+    if (showStatus === false) {
+      this.setState({ showStatus: true });
+    } else {
+      this.setState({ showStatus: false });
+    }
+  }
+
   render() {
+    const { showStatus } = this.state;
     return (
       <div className="admin-wrapper">
         <div className="info-wrapper">
-          <img className="profile-pic" src="http://res.cloudinary.com/bfrest/image/upload/v1519392910/mainPic.png" />
-          <p className="nickName">
+          <img className="profile-pic" src="https://randomuser.me/api/portraits/men/9.jpg" />
+          <p className="name">
             {this.state.firstName} {this.state.lastName}
           </p>
         </div>
 
-        <ul className="options-list">
-          <li>set Status</li>
-          <li>Sign Out</li>
-        </ul>
+        <section className="options-list">
+          <ul>
+            <li className="status-update" onClick={() => this.showStatus()}>
+              Set Status
+            </li>
+            <li>Sign Out</li>
+          </ul>
+        </section>
+        {showStatus === true && <Status />}
       </div>
     );
   }
