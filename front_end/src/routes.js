8d207925b476bf724e06eb23e5cf7b1bc1a3e51f
bfrest <freston75@gmail.com>
2018-08-29 20:41:34 -0600

    working on how to implement context into each component, and got random gifs working!

--- front_end/src/routes.js
+++ front_end/src/routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import ChatBoard from "./components/ChatBoard/ChatBoard.js";
 import CreateUser from "./components/CreateUser/CreateUser.js";
+import Admin from "./components/Admin/Admin.js";
 
 export default (
   <div>
     <Route exact path="/" component={ChatBoard} />
     <Route path="/create-account" component={CreateUser} />
+    <Route path="/admin" component={Admin} />
   </div>
 );
