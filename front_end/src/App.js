1972075425d93feb4d6fedec353b648faf260233
Brent Whitehead <bheadwhite@gmail.com>
2018-09-01 13:09:11 -0600

    cleaned up

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import routes from './routes'
-import './App.css';
+import React, { Component } from "react";
+import routes from "./routes";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      {routes}
-      </div>
-    );
+    return <div className="App">{routes}</div>;
   }
 }
 
