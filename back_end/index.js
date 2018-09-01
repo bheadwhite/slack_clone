25408c21b77d8c94c1f472f9614f981bb88c7a4c
Brent Whitehead <bheadwhite@gmail.com>
2018-09-01 13:08:41 -0600

    cleaned up

--- back_end/index.js
+++ back_end/index.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const cors = require("cors");
 const massive = require("massive");
 const app = express();
-const schema = require("./schema");
 
 app.use("/graphql", cors(), bodyParser.json())
 
@@ -36,15 +35,7 @@ app.use("/graphql", cors(), bodyParser.json())
                                                                                    
  
 */
-massive({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  ssl: true,
-  poolSize: 10
-}).then(resp => {
+massive(process.env.CONNECTIONSTRING).then(resp => {
   app.set('db', resp)
 
   app.listen({ port: 4000 }, () => {
