dd7724490db78f53c0b2a785c280681ec4690fff
bfrest <freston75@gmail.com>
2018-09-05 19:40:07 -0600

    no changes

--- front_end/src/components/ChatBoard/ChatBoard.js
+++ front_end/src/components/ChatBoard/ChatBoard.js
@@ -1,80 +1,41 @@
 import React, { Component } from "react";
-import './Chatboard.css';
-import './ChatMessage/message.css';
-import Nav from '../Nav/Nav'
-
-import { MessageContext } from "../../Contexts/MessageProvider";
+import "./Chatboard.css";
+import Message from "./ChatMessage/Message";
+import Nav from "../Nav/Nav";
 
 class ChatBoard extends Component {
   constructor() {
     super();
     this.state = {
-      firstName: "Brendon",
-      lastName: "Freston",
-      email: "freston75@gmail.com",
-      profileImg: "http://res.cloudinary.com/bfrest/image/upload/v1519392910/mainPic.png",
-      stillImg: "",
-      showStatus: false,
-      messages: ['yo dude what are you doing?', 'wazzzzzuppppppp'],
-      text: '',
+      messages: [],
+      text: ""
     };
 
-    this.submitMessage = this.submitMessage.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.submitMessage = this.submitMessage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
     this.setState({
       text: event.target.value
-    })
+    });
   }
 
-  submitMessage = (event) => {
-    this.state.messages.push(this.state.text)
-    this.setState({
-      text: ''
-    })
-    console.log(this.state.messages)
+  submitMessage(event) {
+    console.log(this.state.text);
   }
 
   editMessage = (id, text) => {
     // edit message functionality
-    console.log(id)
-  }
+    console.log(id);
+  };
 
-  removeMessage = (id) => {
+  removeMessage = id => {
     // remove message functionality
-    console.log(id)
-  }
-
-  channelDisplay = () => {
-    if (this.state.channel) {
-      return `Message ${this.state.channel}`
-    } else {
-      return 'Message Channel'
-    }
-  }
+    console.log(id);
+  };
 
   render() {
-
-    const messageList = this.state.messages.map((message, i) => (
-      <div key={i} className='Message-container'>
-        <div>
-          <img id='profile-img' src='https://www.f6s.com/images/profile-placeholder-user.jpg' />
-        </div>
-        <div className='username'>
-          {this.state.firstName} {this.state.lastName}
-          <div className='Message-text'>
-            {message}
-          </div>
-        </div>
-
-
-        <span className="Message-edit"> ... </span>
-        <span className="Message-delete" onClick={() => this.removeMessage()}> X </span>
-      </div>
-    ))
-
     return (
       <div>
         <Nav />
@@ -82,41 +43,24 @@ class ChatBoard extends Component {
         <div className="ChatBoard-container">
           <div className="ChatBoard-message-parent-container">
             <div className="ChatBoard-message-child-container">
-              <MessageContext.Consumer>
-                {context => (
-                  context.state.messages.map((message, i) => (
-                    <div key={i} className='Message-container'>
-                      <div>
-                        <img id='profile-img' src={context.state.profileImg} />
-                      </div>
-                      <div className='username'>
-                        {context.state.firstName} {context.state.lastName}
-                        <div className='Message-text'>
-                          {message}
-                        </div>
-                      </div>
-
-
-                      <span className="Message-edit"> ... </span>
-                      <span className="Message-delete" onClick={() => this.removeMessage()}> X </span>
-                    </div>
-                  ))
-                )}
-              </MessageContext.Consumer>
+              {this.state.messages.map(message => (
+                <Message id={message.id} key={message.id} text={message.text} time={message.time} edit={this.editMessage} remove={this.removeMessage} />
+              ))}
             </div>
           </div>
           <div className="ChatBoard-message-container">
-            <form className='form-message-container' onSubmit={this.submitMessage} >
-              <input placeholder={this.channelDisplay()}
+            <form className="form-message-container" onSubmit={this.submitMessage}>
+              <input
+                placeholder="Message Channel"
                 onChange={this.handleChange}
-                value={this.state.text}
+                // value={this.state.text}
               />
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ChatBoard
+export default ChatBoard;
