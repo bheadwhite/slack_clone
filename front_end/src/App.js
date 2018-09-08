b2e769597d22a8c1d22435f57ef998b3f08ed0e1
Jon Miller <tallboydesigns@gmail.com>
2018-09-08 11:58:34 -0500

    updated code to reflect backend to frontend error

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import routes from "./routes";
 import "./App.css";
 import UserProvider from "./Contexts/UserProvider.js";
+import MessageProvider from "./Contexts/MessageProvider";
 
 class App extends Component {
   render() {
     return (
-      <UserProvider>
-        <div className="App">{routes}</div>
-      </UserProvider>
+      <MessageProvider>
+        <UserProvider>
+          <div className="App">{routes}</div>
+        </UserProvider>
+      </MessageProvider>
     );
   }
 }
--- front_end/src/Contexts/MessageProvider.js
+++ front_end/src/Contexts/MessageProvider.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 //this will create a new context
 export const MessageContext = React.createContext();
 
 class MessageProvider extends Component {
   state = {
-    firstName: "Brendon",
-    lastName: "Freston",
-    email: "freston75@gmail.com",
-    profileImg: "http://res.cloudinary.com/bfrest/image/upload/v1519392910/mainPic.png",
+    firstName: "",
+    lastName: "",
+    email: "@gmail.com",
+    profileImg: "",
     stillImg: "",
-    showStatus: false,
-    messages: ['yo dude what are you doing?', 'wazzzzzuppppppp'],
-    text: '',
+    showStatus: "",
+    messages: [],
+    text: "",
   };
+
+  componentDidMount() {
+    axios.get('/api/messages').then(res => {
+      this.setState({
+        firstName: res.data[1].first_name,
+        lastName: res.data[1].last_name,
+        email: res.data[1].email,
+        profileImg: res.data[1].profile_img,
+        stillImg: "",
+        showStatus: false,
+        messages: [res.data[1].message],
+        text: '',
+      })
+    })
+  }
 
   render() {
     return (
