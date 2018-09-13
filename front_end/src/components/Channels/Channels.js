70ff142527964a43c7b1870dfede8240f501de03
Bdub <bheadwhite@gmail.com>
2018-09-12 20:11:40 -0600

    added stuff

--- front_end/src/components/Channels/Channels.js
+++ front_end/src/components/Channels/Channels.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 // =========== components ================ //
 import Admin from "./../Admin/Admin";
-
-import "./Channels.css";
+import { UserContext } from "../../Contexts/UserProvider";
 class Channels extends Component {
-  state = {};
+  state = {
+    adminPanelShow: false
+  };
   render() {
+    const { adminPanelShow } = this.state
     return (
       <div className="channels-container">
-        <div className="adminPanel">
+        <div className='adminPanel'
+        onClick={()=>this.setState({adminPanelShow: !this.state.adminPanelShow})}
+        >
+        <h4>DevMountain</h4>
+        <UserContext.Consumer>
+          {context => context.state.firstName}
+          </UserContext.Consumer>
+          <div className={adminPanelShow ? 'adminShow' : 'adminHide'}>
           <Admin />
+          </div>
         </div>
-        <div className="allUnreadsThreads"> all unreads all threads </div>
-        <div className="starred">starred list</div>
-        <div className="channelsSection"> channels section </div>
-        <div className="directMessages"> direct Messages </div>
+        <div className="channelsSection">
+        <div><input type='text'/>search</div>
+          <div className="allUnreadsThreads"> all unreads all threads </div>
+          <div className="starred">starred list</div>
+          <div className="channels"> channels section </div>
+          <div className="directMessages"> direct Messages </div>
+        </div>
 
         <p>Channels component</p>
       </div>
