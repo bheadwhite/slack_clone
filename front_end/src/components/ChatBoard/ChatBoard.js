3fd28d5f6323f6971ae00ec5263a42d03ab8a8a8
Jon Miller <tallboydesigns@gmail.com>
2018-09-14 23:27:35 -0500

    updating styles, still in progress

--- front_end/src/components/ChatBoard/ChatBoard.js
+++ front_end/src/components/ChatBoard/ChatBoard.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import Moment from "react-moment";
-import "./Chatboard.css";
-import "./ChatMessage/message.css";
 import Nav from "../Nav/Nav";
 import Channels from "./../Channels/Channels";
 import axios from "axios";
@@ -85,19 +83,6 @@ class ChatBoard extends Component {
     socket.on("message", this.handleMessage);
   }
 
-<<<<<<< HEAD
-  handleMessage(message) {
-    this.setState({
-      text: message
-    });
-  }
-
-  sendMessage = () => {
-    socket.emit("message", this.state.text);
-  };
-
-=======
->>>>>>> master
   componentWillMount() {
     this.setState({ profile: {} });
     const { userProfile, getProfile } = this.props.auth;
